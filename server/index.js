8bd1326f57b476448693fe2e28b9d95b77faa1c4
zhengfangfang <lizhi@zhengfangfang.local>
2024-11-12 14:56:48 +0800

    feat: Add node server.

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+router.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+const port = 3000;
+const app = express();
+app.use(router);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
