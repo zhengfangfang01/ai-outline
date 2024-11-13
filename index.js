ac184d3c0e458b917d301efc5b0cea2f2bcf803e
zhengfangfang <lizhi@zhengfangfang.local>
2024-11-13 20:54:51 +0800

    feat: test.

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const port = 3003;
 
 app.use(express.static(path.join(__dirname, "./public")));
 
+app.use('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.use('/test', (req, res) => {
+  res.send('test');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
